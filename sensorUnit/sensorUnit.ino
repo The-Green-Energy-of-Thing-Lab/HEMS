691acaf951f2bb9cfbc63a4449839a0403d0ab9c
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2020-10-28 15:22:14 +0700

    mdns

--- sensorUnit/sensorUnit.ino
+++ sensorUnit/sensorUnit.ino
@@ -1,10 +1,13 @@
 #include <WiFi.h>
+#include <ESPmDNS.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ElegantOTA.h>
 
 const char* ssid = "chuwi10";
 const char* password = "1234567890";
+
+String webViewMsg;
 
 WebServer server(80);
 
@@ -26,13 +29,21 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  if (!MDNS.begin("esp32")) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
   server.on("/", []() {
-    server.send(200, "text/plain", "Hi! I am ESP32.");
+    webViewMsg = "Hi! ESP32.";
+    server.send(200, "text/plain", webViewMsg);
   });
 
   ElegantOTA.begin(&server);    // Start ElegantOTA
   server.begin();
   Serial.println("HTTP server started");
+
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
 }
 
 void loop(void) {
